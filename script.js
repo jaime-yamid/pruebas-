ed682b2c4d5f4e341b80d61166a5d05525f39ab2
Jaime Yamid Cabrera <yamid9908@gmail.com>
2023-09-11 09:45:54 -0500

    primer modigficacion y prueba

---
+++ script.js
@@ -0,0 +1,11 @@
+const h1= document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito= document.querySelector('.parrafito');
+const pid= document.querySelector('#pid');
+
+console.log ({
+h1,
+p,
+parrafito,
+pid,
+});
