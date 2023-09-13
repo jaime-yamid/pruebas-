4519b132c37a5e0d13568c70af7b931f10916b3d
Jaime Yamid Cabrera <yamid9908@gmail.com>
2023-09-13 15:52:18 -0500

    le agrego el tema del parrafito

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const h1= document.querySelector('h1');
 const p = document.querySelector('p');
 const parrafito= document.querySelector('.parrafito');
 const pid= document.querySelector('#pid');
+const parrafito2= document.querySelector('.parrafito2');
+
 
 console.log ({
 h1,
 p,
 parrafito,
 pid,
+paraffito2,
 });
