39a9e92754b7bee59b02293c4dc26f937bc3ecae
Jaime Yamid Cabrera <yamid9908@gmail.com>
2023-09-18 16:23:52 -0500

    ultima modificacion de manejo del DOM con HTML

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const h1= document.querySelector('h1');
 const p = document.querySelector('p');
-const parrafito= document.querySelector('.parrafito');
-const pid= document.querySelector('#pid');
-const parrafito2= document.querySelector('.parrafito2');
+const parrafito = document.getElementsByClassName('parrafito');
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
 
-
-console.log ({
+console.log(input.value);
+console.log({
 h1,
 p,
 parrafito,
 pid,
-paraffito2,
+input,
+
 });
+
+h1.innerHTML = 'patito <br> feo';
+console.log(h1.getAttribute('pantalla'));
+//H1.setAttribute 
+
+h1.classList.add('rojo');
+h1.classList.remove('verde');
+
+
+const img = document.createElement('img');
+img.setAttribute('src','https://www.trecebits.com/wp-content/uploads/2022/12/robot.webp');
+
+pid.appendChild(img);
